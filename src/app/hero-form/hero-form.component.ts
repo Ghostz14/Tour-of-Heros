b54b5443431d56ff1c0e07781d5fbb38448ce8e7
Nicolas12-maker <nicolasxpsilvaxp@gmail.com>
2023-02-17 09:37:23 -0300

    Tour_of_Heros

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent {
+
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer', 'Condição', 'Sobrenatural',
+            'Super Força', 'Super Resistência', 'Super Velocidade',];
+
+  model = new Hero(18, 'Hero', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+}
